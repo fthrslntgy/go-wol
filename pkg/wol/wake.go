28e16cde0f5fc6001d9d80b72a98d99ce256508f
Zeki Ahmet Bayar <54356667+zekiahmetbayar@users.noreply.github.com>
2023-02-07 13:58:20 +0300

    Removed unnecessary information lines

--- pkg/wol/wake.go
+++ pkg/wol/wake.go
@@ -9,11 +9,9 @@ import (
 )
 
 func Wake(macAddr string, bcastAddr string, bcastPort any) error {
-
 	if bcastPort == nil {
 		bcastPort = 9
 	}
-
 	host := fmt.Sprintf("%s:%d", bcastAddr, bcastPort)
 	udpAddr, err := net.ResolveUDPAddr("udp", host)
 	if err != nil {
@@ -24,19 +22,13 @@ func Wake(macAddr string, bcastAddr string, bcastPort any) error {
 	if err != nil {
 		return err
 	}
-
 	bs, err := mp.Marshal()
 	if err != nil {
 		return err
 	}
-
-	fmt.Printf("Broadcasting to: %s\n", host)
-
 	err = connection.Connection(udpAddr, bs)
 	if err != nil {
 		return err
 	}
-
-	fmt.Printf("Magic packet sent successfully to %s\n", macAddr)
 	return nil
 }
